2defa38ad6dba29c709de793d41c79fb74437527
fredLeeJH <fred54755475@gmail.com>
2025-11-23 18:42:29 +0900

    #13 [feat] : 컴포넌트 위치 변경

--- core/designsystem/src/main/java/com/umcspot/spot/designsystem/component/button/SocialLoginButton.kt
+++ core/designsystem/src/main/java/com/umcspot/spot/designsystem/component/button/SocialLoginButton.kt
@@ -1,4 +1,4 @@
-package com.umcspot.spot.designsystem.component
+package com.umcspot.spot.designsystem.component.button
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
@@ -22,7 +22,6 @@ import androidx.compose.ui.unit.sp
 import com.umcspot.spot.designsystem.theme.*
 import com.umcspot.spot.designsystem.R
 import com.umcspot.spot.designsystem.shapes.SpotShapes
-import com.umcspot.spot.designsystem.theme.SpotTypography
 
 // ===== 공통 베이스 버튼 =====
 @Composable
