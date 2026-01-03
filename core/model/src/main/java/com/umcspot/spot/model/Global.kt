4b6d9b3a079a01ce93ad04790979232c2b5bf7f4
fredLeeJH <fred54755475@gmail.com>
2026-01-03 18:23:29 +0900

    #18[feat] : 스터디 스타일 enum class 추가

--- core/model/src/main/java/com/umcspot/spot/model/Global.kt
+++ core/model/src/main/java/com/umcspot/spot/model/Global.kt
@@ -6,7 +6,7 @@ enum class WeatherType { HEAVYRAIN, RAIN, SNOW, WIND, COLD, HOT, SUNNY }
 
 enum class SortType { LIVE, RECOMMEND, COMMENTS }
 
-enum class BoardType { PASSREVIEW, INFOSHARE, CONSULT, JOBTALK, FREETALK}
+enum class BoardType { PASSREVIEW, INFOSHARE, CONSULT, JOBTALK, FREETALK }
 
 enum class RecruitingStudySort(val label: String) {
     LATEST("최신 순"),
@@ -17,16 +17,16 @@ enum class RecruitingStudySort(val label: String) {
 val BoardType.korean: String
     get() = when (this) {
         BoardType.PASSREVIEW -> "합격후기"
-        BoardType.INFOSHARE  -> "정보공유"
-        BoardType.CONSULT    -> "고민상담"
-        BoardType.JOBTALK    -> "취준토크"
-        BoardType.FREETALK   -> "자유토크"
+        BoardType.INFOSHARE -> "정보공유"
+        BoardType.CONSULT -> "고민상담"
+        BoardType.JOBTALK -> "취준토크"
+        BoardType.FREETALK -> "자유토크"
     }
 
 enum class AlertKind { POPULAR_POST, STUDY_NOTICE, STUDY_SCHEDULE, TODO_DONE }
 
 enum class ActivityType(
-    val label : String
+    val label: String
 ) {
     ONLINE("온라인"),
     OFFLINE("오프라인")
@@ -34,7 +34,7 @@ enum class ActivityType(
 
 enum class FeeRange(
     val label: String
-){
+) {
     NONE("없음"),
     UNDER_10K("1만원 미만"),
     ABOUT10K("1만원대"),
@@ -57,6 +57,19 @@ enum class StudyTheme(
     COMPETITION("공모전"),
     MAJOR_CAREER("전공 / 진로 학습"),
     OTHER("기타")
+}
+
+enum class StudyStyle {
+    NETWORKING,
+    GOAL_OR_RULE_ORIENTED,
+    SHORT_TERM,
+    LONG_TERM,
+    INDIVIDUAL_PLUS_DISCUSSION,
+    GROUP_PLUS_SIMULTANEOUS,
+    LEARNING_BASED,
+    DISCUSSION_BASED,
+    LIGHT_AND_FLEXIBLE,
+    STRUCTURED_AND_PLANNED
 }
 
 enum class SocialLoginType(
