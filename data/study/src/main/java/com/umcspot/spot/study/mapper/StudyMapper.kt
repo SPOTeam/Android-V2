c4e864f2bd84a3f9de38db9cf5b68a50d32f71f9
fredLeeJH <fred54755475@gmail.com>
2026-01-04 09:54:19 +0900

    #18[feat] : Mapper 수정

--- data/study/src/main/java/com/umcspot/spot/study/mapper/StudyMapper.kt
+++ data/study/src/main/java/com/umcspot/spot/study/mapper/StudyMapper.kt
@@ -1,40 +1,34 @@
 package com.umcspot.spot.study.mapper
 
 import com.umcspot.spot.study.dto.request.StudyRequestDto
-import com.umcspot.spot.study.dto.response.CreateStudyResponseDto
 import com.umcspot.spot.study.dto.response.Study as DTOStudy
 import com.umcspot.spot.study.dto.response.StudyResponseDto
 import com.umcspot.spot.study.model.StudyCreateModel
 import com.umcspot.spot.study.model.StudyResult
 import com.umcspot.spot.study.model.StudyResultList
 
-fun StudyCreateModel.toData(): StudyRequestDto {
-    return StudyRequestDto(
-        name = this.name,
-        maxMembers = this.maxMembers,
-        hasFee = this.hasFee,
-        amount = this.amount,
-        description = this.description,
-        categories = this.categories, 
-        styles = this.styles,         
-        regionCodes = this.regionCodes
-    )
-}
+fun StudyCreateModel.toData(): StudyRequestDto = StudyRequestDto(
+    name = this.name,
+    maxMembers = this.maxMembers,
+    hasFee = this.hasFee,
+    amount = this.amount,
+    description = this.description,
+    categories = this.categories,
+    styles = this.styles,
+    regionCodes = this.regionCodes
+)
 
-fun DTOStudy.toDomain() : StudyResult =
-    StudyResult (
-        studyId = this.studyId,
-        title = this.title,
-        goal = this.goal,
-        maxMember = this.maxMember,
-        member = this.member,
-        likes = this.likes,
-        views = this.views,
-        studyImage = this.studyImage
-    )
+fun DTOStudy.toDomain(): StudyResult = StudyResult(
+    studyId = this.studyId,
+    title = this.title,
+    goal = this.goal,
+    maxMember = this.maxMember,
+    member = this.member,
+    likes = this.likes,
+    views = this.views,
+    studyImage = this.studyImage
+)
 
-fun StudyResponseDto.toDomainList(): StudyResultList =
-    StudyResultList(
-        studyList = this.studyList.map(DTOStudy::toDomain)
-    )
-fun CreateStudyResponseDto.toDomain(): Long = this.studyId
+fun StudyResponseDto.toDomain(): StudyResultList = StudyResultList(
+    studyList = this.studyList.map { it.toDomain() }
+)
