95d500bb12a1723fe83c07d2d464ad62801e4151
yeonwoo <chooyw100@gmail.com>
2026-01-07 16:43:34 +0900

    fix : remove useless import

--- data/user/src/main/java/com/umcspot/spot/user/dto/response/UserPreferredRegionResponseDto.kt
+++ data/user/src/main/java/com/umcspot/spot/user/dto/response/UserPreferredRegionResponseDto.kt
@@ -1,7 +1,6 @@
 package com.umcspot.spot.user.dto.response
 
 import android.annotation.SuppressLint
-import com.umcspot.spot.model.WeatherType
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
