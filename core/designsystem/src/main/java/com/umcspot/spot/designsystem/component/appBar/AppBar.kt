1dc6c480215ffb5f86f7cdcd132488dfee69493c
yeonwoo <chooyw100@gmail.com>
2026-01-07 14:04:25 +0900

    fix : TopBar 사이즈 변경

--- core/designsystem/src/main/java/com/umcspot/spot/designsystem/component/appBar/AppBar.kt
+++ core/designsystem/src/main/java/com/umcspot/spot/designsystem/component/appBar/AppBar.kt
@@ -38,8 +38,8 @@ import com.umcspot.spot.designsystem.theme.G300
 import com.umcspot.spot.designsystem.theme.G400
 import com.umcspot.spot.designsystem.theme.SpotTheme
 import com.umcspot.spot.designsystem.theme.White
+import com.umcspot.spot.ui.extension.screenHeightDp
 import com.umcspot.spot.ui.extension.screenWidthDp
-
 
 @Composable
 fun AppBarHome (
@@ -51,35 +51,40 @@ fun AppBarHome (
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .height(screenHeightDp(53.dp))
             .background(SpotTheme.colors.white)
-            .padding(horizontal = 16.dp, vertical = 8.dp),
+            .padding(horizontal = screenWidthDp(17.dp)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        
         Image(
             painter = painterResource(id = R.drawable.spot_logo),
             contentDescription = "App Logo",
-            modifier = Modifier.size(32.dp)
+            modifier = Modifier.size(screenWidthDp(33.dp))
         )
 
         Row(verticalAlignment = Alignment.CenterVertically) {
-            IconButton(onClick = onSearchClick) {
+            IconButton(
+                modifier = Modifier.size(screenWidthDp(32.dp)),
+                onClick = onSearchClick) {
                 Icon(
                     painter = painterResource(id = R.drawable.search),
                     contentDescription = "Search",
-                    modifier = Modifier.size(32.dp)
+                    modifier = Modifier.size(screenWidthDp(24.dp))
                 )
             }
-            Spacer(modifier = Modifier.width(8.dp))
-            IconButton(onClick = onAlertClick) {
+            Spacer(modifier = Modifier.width(screenWidthDp(8.dp)))
+            IconButton(
+                modifier = Modifier.size(screenWidthDp(32.dp)),
+                onClick = onAlertClick
+            ) {
                 Icon(
                     painter = painterResource(
                         id = if (hasAlert) R.drawable.alert_noti else R.drawable.alert
                     ),
                     contentDescription = if (hasAlert) "New Notifications" else "Notifications",
                     tint = Color.Unspecified,
-                    modifier = Modifier.size(32.dp)
+                    modifier = Modifier.size(24.dp)
                 )
             }
         }
@@ -119,21 +124,24 @@ fun BackTopBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .height(screenHeightDp(53.dp))
             .background(SpotTheme.colors.white)
-            .padding(start = 5.dp, top = 16.dp, bottom = 16.dp),
+            .padding(start = screenWidthDp(17.dp)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
     ) {
         IconButton(
-            onClick = onBackClick,
+            modifier = Modifier.size(screenWidthDp(20.dp)),
+            onClick = onBackClick
         ) {
             Icon(
+                modifier = Modifier.size(screenWidthDp(20.dp)),
                 painter = painterResource(id = R.drawable.arrow_left),
-                contentDescription = "Back" 
+                contentDescription = "Back"
             )
         }
 
-        Spacer(modifier = Modifier.width((-2).dp))
+        Spacer(modifier = Modifier.width(screenWidthDp(10.dp)))
 
         Text(
             text = title,
@@ -238,7 +246,6 @@ fun SearchTopBar(
 @Composable
 fun PreviewSearchTopBarWithText() {
     var text by remember { mutableStateOf("안녕하세요") }
-
     SpotTheme{
         SearchTopBar(
             value = text,
