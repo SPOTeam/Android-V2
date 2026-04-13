5268a4a88f1421fdf837f8ba1d0784a1d50644af
fredLeeJH <fred54755475@gmail.com>
2026-04-13 23:59:03 +0900

    #49[feat] : 선호 카테고리, 지역 화면 추가

--- feature/study/src/main/java/com/umcspot/spot/study/preferLocation/PreferLocationStudyScreen.kt
+++ feature/study/src/main/java/com/umcspot/spot/study/preferLocation/PreferLocationStudyScreen.kt
@@ -231,7 +231,7 @@ fun PreferLocationStudyScreen(
                     } else {
                         EmptyAlert(
                             modifier = Modifier.fillMaxSize(),
-                            painter = painterResource(R.drawable.emoji_sad),
+                            painter = painterResource(R.drawable.ic_sad),
                             alertTitle = "조건에 맞는 스터디가 없어요.",
                             alertDes = "필터를 재설정하고 스터디를 찾아보세요.",
                         )
